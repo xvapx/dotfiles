eabd87954c51e9eb1bba2f4192ec7a785721602a
Marti Serra <marti.serra.coscollano@gmail.com>
2017-07-15 22:33:17 +0200

    added conky

--- default.nix
+++ default.nix
@@ -14,5 +14,9 @@ stdenv.mkDerivation rec{
     cp ./bash/bash_profile $out/.bash_profile
     cp ./git/gitconfig $out/.gitconfig
     cp -r ./nixos $out/nixos
+    mkdir -p $out/.config/conky
+    cp ./conky/conky.conf $out/.config/conky/conky.conf
+    mkdir -p $out/.config/autostart
+    cp ./conky/conky.desktop $out/.config/autostart/conky.desktop
   '';
 }
