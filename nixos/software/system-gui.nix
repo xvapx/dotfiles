4cf338c483a14ca963f636c00bf5c7c9f259568b
Marti Serra <marti.serra.coscollano@gmail.com>
2017-07-30 19:20:00 +0200

    fixed veracrypt

--- nixos/software/system-gui.nix
+++ nixos/software/system-gui.nix
@@ -9,7 +9,7 @@ with default; with channels;[
   mupdf                             # Lightweight PDF, XPS, and E-book viewer
   gparted                           # Graphical partition editor
   terminator                        # Terminal emulator which supports tabs and multiple resizable terminal panels in one window
-  veracrypt                         # Disk encryption software
+  nixpkgs-master.veracrypt          # Disk encryption software
   xclip                             # CLI to X selections
   xdotool                           # Fake keyboard/mouse input, window management, and more
 ]
